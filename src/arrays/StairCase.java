0f5b01e4b37fcefc6cbc9cc65914cd6dec17e320
TE1 <TE1@192.168.0.5>
2018-07-27 21:55:11 +0530

    Print # like #   ##  ### ####

---
+++ src/arrays/StairCase.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class StairCase {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+	    for(int i=1;i <=n; i++) {
+	    	for(int j=n;j>=1;j--) {
+	    		if(j<=i) {
+	    			System.out.print("#");
+	    		}else {
+	    			System.out.print(" ");
+	    		}
+	    		
+	    	}
+	    	System.out.println();
+	    }
+	}
+
+	
+}
