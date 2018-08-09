ced68876daa864c9cb725a409018e1d40d33ea3c
TE1 <TE1@192.168.0.5>
2018-08-09 17:41:49 +0530

    Recursion Practice

--- src/recursion/ShuffleIntegers.java
+++ src/recursion/ShuffleIntegers.java
@@ -1,31 +1,33 @@
 package recursion;
 
+/*package whatever //do not write package name here */
+
 import java.util.Scanner;
 
-public class ShuffleIntegers {
- public static void main(String[] args) {
-	 Scanner scanner = new Scanner(System.in);
-	 int noOfTestCases = scanner.nextInt();
-	 while(noOfTestCases-- != 0) {
-		 int n = scanner.nextInt();
-		 int[] arr = new int[n];
-		 for(int i=0;i<n;i++) {
-			 arr[i] = scanner.nextInt();
-		 }
-		 shuffle(arr, 0, n/2);
-	 }
-	 scanner.close();
- }
-
-private static int[] shuffle(int[] arr, int i, int n) {
-	
-	    if(n==arr.length) {
-		  return arr;
+class ShuffleIntegers {
+	public static void main (String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int noOfTestcases = scanner.nextInt();
+		while(noOfTestcases-- !=0){
+		    int n = scanner.nextInt();
+		    int[] arr = new int[n];
+		    for(int i = 0;i <n; i++){
+		        arr[i] = scanner.nextInt();
+		    }
+		    shuffle(arr,0,n/2);
+		}
+	}
+	public static void shuffle(int[] arr, int firstPointerIndex, int lastPointerIndex){
+	   
+	    int n = arr.length;
+	    if(firstPointerIndex == n/2 || lastPointerIndex == n){
+	        
+	        if(lastPointerIndex == n-1 )
+	         System.out.print(arr[lastPointerIndex]);
+	        System.out.println();
+	        return;
 	    }
-		System.out.print(arr[i]+" ");
-		System.out.print(arr[n]+" ");
-		i++;
-	    return shuffle(arr, i , (arr.length/2)+i);
-}
-
+	    System.out.print(arr[firstPointerIndex]+" "+arr[lastPointerIndex]+" ");
+	    shuffle(arr, firstPointerIndex+1, lastPointerIndex+1);
+	}
 }
